cfbaafdea028a7ba00833652bbddc77f94806fdf
Slick <holygalaxy444@gmail.com>
2021-09-23 18:42:52 +0500

    cinema

--- cinema/front/src/App.js
+++ cinema/front/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({
+      'dev': [],
+      'films': [],
+      'schedule': []
+    })
+  }
+
+  render() {
+    console.log('state', this.state);
+    return(
+        <div>
+          Cinema
+        </div>
+    )
+  }
+
 }
 
 export default App;
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Project
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
