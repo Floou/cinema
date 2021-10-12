8a51cbc2efb8d5b50fca5a0db4807c8dbda3269c
Slick <holygalaxy444@gmail.com>
2021-10-12 08:56:20 +0500

    hw

--- cinema/front/src/components/FilmDetail.js
+++ cinema/front/src/components/FilmDetail.js
@@ -3,8 +3,10 @@ import {useParams} from "react-router";
 
 const FilmDetail = ({films}) => {
     let {id} = useParams();
-    console.log('id:',id);
-    console.log('films:',films);
+    // console.log('id:',id, typeof id, typeof parseInt(id), typeof +id);
+    let film = films.filter((item) => item.id === +id)[0];
+    console.log('this film:', film);
+
     return (
         <h3>Film</h3>
     )
