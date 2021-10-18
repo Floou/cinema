bbe4c706217a01e6313862f8b44ae719b02dc35f
Slick <holygalaxy444@gmail.com>
2021-10-18 18:45:14 +0500

    hw

--- cinema/front/src/App.js
+++ cinema/front/src/App.js
@@ -11,122 +11,6 @@ import Main from "./components/Main";
 import axios from "axios";
 
 
-
-
-const UsersMock = [
-    {
-    "id": 1,
-    "username": "user1",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 2,
-    "username": "user2",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 3,
-    "username": "user3",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 4,
-    "username": "user4",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 5,
-    "username": "user5",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  }
-];
-
-const ScheduleMock = [{
-    "id": 1,
-    "date_time": "2021-09-30T12:00:00Z",
-    "film": 1
-  },
-  {
-    "id": 2,
-    "date_time": "2021-10-03T11:00:00Z",
-    "film": 2
-  },
-  {
-    "id": 3,
-    "date_time": "2021-12-11T18:30:00Z",
-    "film": 4
-  }
-];
-
-const FilmsMock = [
-    {
-    "id": 1,
-    "title": "Плохие парни навсегда",
-    "description": "Детективы Майк Лоури и Маркус Бёрнетт снова в деле!",
-    "audience": [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      11
-    ]
-  },
-  {
-    "id": 2,
-    "title": "Гнев человеческий",
-    "description": "Эйч — загадочный и холодный на вид джентльмен, но внутри него пылает жажда мести. Преследуя свои мотивы, он внедряется в инкассаторскую компанию, чтобы выйти на соучастников серии многомиллионных ограблений, потрясших Лос-Анджелес. В этой запутанной игре у каждого своя роль, но под подозрением оказываются все. Виновных же обязательно постигнет гнев человеческий.",
-    "audience": [
-      1,
-      3,
-      4,
-      7,
-      8,
-      11
-    ]
-  },
-  {
-    "id": 3,
-    "title": "Человек-паук: Вдали от дома",
-    "description": "Питер Паркер вместе с одноклассниками отправляется на летние каникулы в Европу. Однако отдохнуть приятелям вряд ли удастся - по прибытии в Венецию группа попадет в эпицентр сражения. Некое существо, состоящее из воды, появляется из ниоткуда и начинает крушить всё на своём пути, и теперь Питеру придётся спасать европейские архитектурные памятники вместе с загадочным супергероем Мистерио.",
-    "audience": [
-      1,
-      2,
-      3,
-      5,
-      7,
-      8,
-      11
-    ]
-  }
-];
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -140,17 +24,31 @@ class App extends React.Component {
       componentDidMount() {
 
       axios
-            .get("http://localhost:8000/api/users/")
+            .get("http://127.0.0.1:8000/api/users")
             .then((result) => {
-                console.log('result', result)
+                // console.log('users result:', result)
+                this.setState({
+                    users: result.data
+                })
+            })
+            .catch((error) => console.log(error));
+        axios
+            .get("http://127.0.0.1:8000/api/films")
+            .then((result) => {
+                this.setState({
+                    films: result.data
+                })
+            })
+            .catch((error) => console.log(error));
+        axios
+            .get("http://127.0.0.1:8000/api/schedule")
+            .then((result) => {
+                this.setState({
+                    schedules: result.data
+                })
             })
             .catch((error) => console.log(error));
 
-        this.setState({
-            users: UsersMock,
-            films: FilmsMock,
-            schedules: ScheduleMock
-        })
     }
 
   render() {
