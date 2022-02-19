8309dd051bb19fc2040281436b874b4337203138
Floou <70875478+Floou@users.noreply.github.com>
2022-02-19 10:44:54 +0500

    Delete deploy/sources directory

--- deploy/sources/checks/211108_1.py
+++
@@ -1,8 +0,0 @@
-from requests import post
-
-url = 'http://localhost:8000/api-token-auth/'
-user_cred = {'username': 'cinema', 'password': 'admin000'}
-response = post(url, data=user_cred)
-resp_data = response.json()
-# print(dir(response))
-print(resp_data)
